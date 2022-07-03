531274e176b8708e212339ab6185a3b615783c02
Marvis Ijenwa <66011932+IjEmpire@users.noreply.github.com>
2022-07-03 13:33:13 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+
+
+//hamburger menu
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+//home-nav-link color to orangered
+const inputUrl = window.location.href;
+const homeLink = document.getElementById("home-link");
+
+function changeHomeColor() {
+    if (inputUrl == "http://192.168.0.102:5500/index.html") {
+        homeLink.style.color = "orangered";
+    }
+};
+changeHomeColor();
+
+//translate(DE-ENG) welcome msg on skillset paragraph
+const welcomeMsg =document.getElementById("welcome-msg");
+function translateMsg(x){
+    welcomeMsg.innerHTML = "warm welcome! &nbsp &nbsp &nbsp&nbsp&nbsp";
+    welcomeMsg.style.color = "#101010";
+}
+function unTranslateMsg(x){
+    welcomeMsg.innerHTML = "herzlich willkommen!";
+    welcomeMsg.style.color = "#69D6F4";
+}
+
+/*
+const welcomeMsg = document.getElementById("welcome-msg");
+const newWelcomemsg = "Warm Welcome"
+welcomeMsg.addEventListener("click", function(){
+    welcomeMsg.innerHTML = newWelcomemsg;
+});
+*/
+
+//goTo resume.io || i decided to use this link instead of 
+//a the local pdf file in /root/docs folder. but I#ll leave the file there
+const cvBtn = document.getElementById("btn2");
+cvBtn.addEventListener("click", function(){
+    window.open("https://resume.io/r/2PLVNcPxy");
+});
+
+//go to URL- contact.index in root folder
+function toContactPage(){
+    location.href="contact.html"
+};
+
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
